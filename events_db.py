82d28b9fa365a77eb1e69f6f0dc6b54d7eee231e
QuintonTorronto <aakarshofficial29@gmail.com>
2025-07-06 23:09:18 +0530

    Handling 500 Server error

--- events_db.py
+++ events_db.py
@@ -14,7 +14,11 @@ def save_event(data):
     collection.insert_one(data)
 
 def get_latest_events(limit=20):
-    return list(collection.find({}, {"_id": 0}).sort("timestamp", -1).limit(limit))
-
+    try:
+        return list(collection.find({}, {"_id": 0}).sort("timestamp", -1).limit(limit))
+    except Exception as e:
+        print("Error reading from DB:", e)
+        return []
+    
 def is_duplicate(request_id):
     return collection.find_one({"request_id": request_id}) is not None
